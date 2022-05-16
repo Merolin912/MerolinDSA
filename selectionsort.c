11f6aed19efc8aa114c8ae76c8ddf12e3f0e0a43
Merolin Thoras <98947359+Merolin912@users.noreply.github.com>
2022-05-16 14:58:22 +0530

    DSA version

---
+++ selectionsort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+int a[20],n,i,j,min;
+printf("Enter a value of n");
+scanf("%d",&n);
+printf("Enter integer values\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n-1;i++)
+{
+for(j=0;j<n-i-1;j++)
+{
+if(a[j]>a[j+1])
+{
+min=a[j];
+a[j]=a[j+1];
+a[j+1]=min;
+}
+}
+}
+printf("Array element in sorted order\n");
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+}
+
+
